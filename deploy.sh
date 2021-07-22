7968a3a94b0386f955ef57c39297b62ca857ee11
15x15G <65473527+15x15G@users.noreply.github.com>
2021-07-22 16:49:43 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push  git@github.com:15x15G/15x15G.github.io.git master
+git push -f git@github.com:15x15G/15x15G.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
