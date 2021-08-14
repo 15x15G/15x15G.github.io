9c12a07070b9e96fd240d62c8193eba6cd3dbb3c
15x15G <65473527+15x15G@users.noreply.github.com>
2021-08-14 15:08:51 +0800

    add robots.txt add some plugin add icon add action

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ https://15x15G.github.io
 
 ## 教程
 
-[官方教程](https://v0.vuepress.vuejs.org/zh/guide/getting-started.html)
+[官方教程](https://v1.vuepress.vuejs.org/zh/guide/getting-started.html)
 
 [其他教程](https://segmentfault.com/a/1190000017055963)
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,11 @@ module.exports = {
     title: '15x15',
     description: '15x15的主页',
     head: [
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+        ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+
         [
             'script', // js 文件
             { src: '/js/eoclock.js' }
@@ -50,6 +55,51 @@ module.exports = {
                     '/ff14/AtmaClock'
                 ]
             }
-        ]
+        ],
+        lastUpdated: '最近更新' // string | boolean
+
+    },
+    plugins:
+    {
+        'robots':
+        {
+            /**
+             * @host
+             * Mandatory, You have to provide the host URL
+             */
+            host: "https://15x15G.github.io",
+            /**
+             * @disallowAll
+             * Optional: if it's true, all others options are ignored and exclude all robots from the entire server
+             */
+            disallowAll: true,
+            /**
+             * @allowAll
+             * Optional: if it's true and @disallowAll is false, all others options are ignored and allow all robots complete access
+             */
+            allowAll: false,
+            /**
+             * @sitemap
+             * Optional, by default: sitemap.xml
+             */
+            sitemap: "/assets/sitemap.xml"
+            /**
+             * @policies
+             * Optional, by default: null
+             */
+            // policies: [
+            // {
+            //     userAgent: '*',
+            //     disallow: [
+            //         '/admin', '/login'
+            //     ],
+            //     allow: [ // Optional: Allowed paths. 
+            //         'products', 'blog'
+            //     ]
+            // }]
+        },
+        '@vuepress/back-to-top': true,
+        '@vuepress/medium-zoom': true,
+        '@vuepress/nprogress': true
     }
 }
